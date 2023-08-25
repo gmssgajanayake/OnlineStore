7b8b3ca48a48d27c199ee9c1d0a89ebf695d9f82
g.m.sakujagajanayake <sakuja1shamal@gmail.com>
2023-08-25 22:07:12 +0530

    V1

--- Electronics.java
+++ Electronics.java
@@ -1,23 +1,22 @@
-public class Electronics extends Product{
-	private String brand;
-	
-	public Electronics(int id,String name,double price,int quantity,String brand){
-		super(id,name,price,quantity);
-		this.brand=brand;
-		
-	}
-	
-	public String getBrand(){
-		return brand;
-	}
-	
-	public void setBrand(String brand){
-		this.brand=brand;
-	}
-	
-	@Override
-	public String displayInfo(){
-		return super.displayInfo()+"\n\tBrand : "+brand;
-	}
+public class Electronics extends Product {
+    private String brand;
+
+    public Electronics(int id, String name, double price, int quantity, String brand) {
+        super(id, name, price, quantity);
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String displayInfo() {
+        return super.displayInfo() + "\n\tBrand : " + brand;
+    }
 
 }
--- Product.java
+++ Product.java
@@ -1,49 +1,50 @@
-public class Product{
-	private int id;
-	private String name;
-	private double price;
-	private int quantity;
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private int quantity;
 
-	public Product(int id,String name,double price,int quantity){
-		this.id=id;
-		this.name=name;
-		this.price=price;
-		this.quantity=quantity;
-	}
-	
-	public int getId(){
-		return id;
-	}
-	
-	public void setId(int id){
-		this.id=id;
-	}
-	
-	public String getName(){
-		return name;
-	}
-	
-	public void setName(String name){
-		this.name=name;
-	}
-	
-	public double getPrice(){
-		return price;
-	}
-	
-	public void setPrice(double price){
-		this.price=price;
-	}
-	public int getQuantity(){
-		return quantity;
-	}
-	
-	public void setQuantity(int quantity){
-		this.quantity=quantity;
-	}
-	
-	public String displayInfo(){
-		return "Product - \n\tId : "+id+"\n\tName : "+name+"\n\tPrice : $ "+price+"\n\tQuantity : "+getQuantity();
-	}
+    public Product(int id, String name, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String displayInfo() {
+        return "Product - \n\tId : " + id + "\n\tName : " + name + "\n\tPrice : $ " + price + "\n\tQuantity : " + getQuantity();
+    }
 
 }
